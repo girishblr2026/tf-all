812e4070071d4d8bd0a2cbd723b516fda033315f
girishblr2025 <girish.blr2025@gmail.com>
2023-03-12 23:22:40 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-k8s-resources"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2"
+    
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
